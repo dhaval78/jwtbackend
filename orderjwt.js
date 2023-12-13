20a32fd5a885a191011b56b17d6cd2da46874f2f
dhaval78 <dhavi78@gmail.com>
2023-12-13 11:12:09 +0530

    commit

--- orderjwt.js
+++ orderjwt.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 app.use(cors());
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "https://shop-a70cd.web.app");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", "true"); 
